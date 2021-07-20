2d2f85185f57cc73b28e55825ae2d8b25a7d7cef
Ubuntu <ubuntu@ip-172-31-25-98.us-east-2.compute.internal>
2021-07-20 04:58:02 +0000

    vm jul 19

--- model.py
+++ model.py
@@ -234,35 +234,41 @@ class Pure_Bert(nn.Module):
 
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
-        self.layers_num_to_agg = [int(i) for i in args.pure_bert_layer_agg_list.split(',')]
+#         self.layers_num_to_agg = [int(i) for i in args.pure_bert_layer_agg_list.split(',')]
+#         logger.info('layers to agg:{}'.format(self.layers_num_to_agg))
 
-        self.layers = [nn.Linear(config.hidden_size, config.hidden_size) ] * len(self.layers_num_to_agg)
-        # self.layer_12 = nn.Linear(config.hidden_size, config.hidden_size)
-        # self.layer_11 = nn.Linear(config.hidden_size, config.hidden_size)
+        #self.layers = [nn.Linear(config.hidden_size, hidden_size) ] * len(self.layers_num_to_agg)
+        #logger.info('layers :{}'.format(self.layers))
+        self.layer_12 = nn.Linear(config.hidden_size, hidden_size)
+        self.layer_11 = nn.Linear(config.hidden_size, hidden_size)
+        #self.layer_10 = nn.Linear(config.hidden_size, hidden_size)
         
-        logger.info('!!!!!!!!new model!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        logger.info('!!!!!!!!new model!!!!????')
 
 
-        layers = [nn.Linear(
-            config.hidden_size, hidden_size), nn.ReLU(), nn.Linear(hidden_size, args.num_classes)]
+        layers = [nn.ReLU(), nn.Linear(hidden_size, hidden_size), nn.ReLU(), nn.Linear(hidden_size, args.num_classes)]
         self.classifier = nn.Sequential(*layers)
 
     def forward(self, input_ids, token_type_ids):
         outputs = self.bert(input_ids, token_type_ids=token_type_ids)
         
-        #outputs_12 = outputs[2][11][:,1, :]
-        #outputs_11 = outputs[2][10][:,1, :]
-
-        pooled_output = None
-        if len(self.layers_num_to_agg) == 1:
-            pooled_output = outputs[2][self.layers_num_to_agg[0]][:,0, :]
-        else:
-            for i, num in enumerate(self.layers_num_to_agg):
-                #outputs = torch.add(self.layer_12(outputs_12), self.layer_11(outputs_11))
-                if i == 0:
-                    pooled_output = self.layers[i](outputs[2][num][:,0,:])
-                else:
-                    pooled_output = torch.add(pooled_output, self.layers[i](outputs[2][num][:,0,:]))
+        outputs_12 = outputs[2][12][:,0, :]
+        outputs_11 = outputs[2][11][:,0, :]
+        #outputs_10 = outputs[2][10][:,0, :]
+        pooled_output = torch.add(self.layer_12(outputs_12), self.layer_11(outputs_11))
+        
+        
+#         pooled_output = None
+#         if len(self.layers_num_to_agg) == 1:
+#             pooled_output = outputs[2][self.layers_num_to_agg[0]][:,0, :]
+#         else:
+#             for i, num in enumerate(self.layers_num_to_agg):
+#                 #outputs = torch.add(self.layer_12(outputs_12), self.layer_11(outputs_11))
+#                 if i == 0:
+                    
+#                     pooled_output = self.layers[i](outputs[2][num][:,0,:])
+#                 else:
+#                     pooled_output = torch.add(pooled_output, self.layers[i](outputs[2][num][:,0,:]))
 
 
 
